22807390f4187280a10572b07a0597a7823a3ffb
Mokshyada Mishra <mokshyada2004@gmail.com>
2025-05-12 16:20:49 +0530

    Initial commit for CNN

---
+++ code.py
@@ -0,0 +1,34 @@
+# Install TensorFlow
+!pip install tensorflow
+
+# Import libraries
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+
+# Load and preprocess data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+X_train, X_test = X_train / 255.0, X_test / 255.0
+y_train_cat, y_test_cat = to_categorical(y_train), to_categorical(y_test)
+
+# Build model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+# Compile and train
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train_cat, epochs=10, validation_data=(X_test, y_test_cat))
+
+# Evaluate
+test_loss, test_acc = model.evaluate(X_test, y_test_cat)
+print("Test accuracy:", test_acc)
